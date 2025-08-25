dd9bf76588ccebaf28fba66e796ebc0a95389a59
vertex616 <yousafbazaz@gmail.com>
2025-08-25 20:42:17 +0100

    Added 3D isometric room asset and formatted it into the main page

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button"
+import HeroExperience from "../components/HeroModels/HeroExperience"
 import { words } from "../constants"
 
 const Hero = () => {
@@ -43,6 +44,11 @@ const Hero = () => {
                 </div>
             </header>
             {/*RIGHT: 3D Model*/}
+            <figure>
+                <div className="hero-3d-layout">
+                    <HeroExperience />
+                </div>
+            </figure>
         </div>
     </section>
 
