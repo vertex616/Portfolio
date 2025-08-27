801264a4ab06b4cc5d29388f21f01e46727eb954
vertex616 <yousafbazaz@gmail.com>
2025-08-27 20:12:23 +0100

    Add new logos and update logo references; remove unused logo files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import LogoSection from './components/LogoSection'
+import NavBar from './components/NavBar'
 import Hero from './sections/Hero'
 import ShowcaseSection from './sections/ShowcaseSection'
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <ShowcaseSection />
+      <LogoSection />
     </>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -37,37 +37,19 @@ const counterItems = [
 
 const logoIconsList = [
   {
-    imgPath: "/images/logos/company-logo-1.png",
+    imgPath: "/images/logos/AstonUniversityLogo.png"
+  },
+  {
+    imgPath: "/images/logos/ZFLogo.png"
   },
   {
     imgPath: "/images/logos/company-logo-2.png",
   },
   {
-    imgPath: "/images/logos/company-logo-3.png",
-  },
-  {
     imgPath: "/images/logos/company-logo-4.png",
   },
   {
-    imgPath: "/images/logos/company-logo-5.png",
-  },
-  {
     imgPath: "/images/logos/company-logo-6.png",
-  },
-  {
-    imgPath: "/images/logos/company-logo-7.png",
-  },
-  {
-    imgPath: "/images/logos/company-logo-8.png",
-  },
-  {
-    imgPath: "/images/logos/company-logo-9.png",
-  },
-  {
-    imgPath: "/images/logos/company-logo-10.png",
-  },
-  {
-    imgPath: "/images/logos/company-logo-11.png",
   },
 ];
 
